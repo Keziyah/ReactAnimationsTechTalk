57621ebb26debc4e2afd0da9ce339485057ec8e6
Keziyah <keziyahlewis@gmail.com>
2017-04-19 21:08:37 -0400

    basic home component complete

---
+++ app/components/Home.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+      return (
+      <div className="container">
+          <nav className="navbar navbar-default">Hello I am a navbar</nav>
+        <div className="row">
+            <div className="col-md-4"><h3>Enlarge Item on Hover</h3></div>
+            <div className="col-md-4"><h3>Delayed Fade-In on load</h3></div>
+            <div className="col-md-4"><h3>Search icon that expands to input box</h3></div>
+        </div>
+        <div className="row">
+            <div className="col-md-4"><h3>Loading animation</h3></div>
+            <div className="col-md-4"><h3>Animated text on load</h3></div>
+            <div className="col-md-4"><h3>Click to enlarge footer</h3></div>
+        </div>
+     <footer className="footer">This is a simple react app. </footer>
+      </div>
+      )
+    }
+}
+
+export default Home
--- app/main.jsx
+++ app/main.jsx
@@ -5,30 +5,15 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import store from './store'
-import Jokes from './components/Jokes'
+import Home from './components/Home'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 import NotFound from './components/NotFound'
 
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-)(
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-)
-
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={ExampleApp}>
-        <IndexRedirect to="/jokes" />
-        <Route path="/jokes" component={Jokes} />
-      </Route>
+      <Route path="/" component={Home}/>
       <Route path='*' component={NotFound} />
     </Router>
   </Provider>,
